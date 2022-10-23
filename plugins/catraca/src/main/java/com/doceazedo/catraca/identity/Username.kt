cbe960c6a1f6a071233d64b2fe2aab3453d86946
doceazedo <me@doceazedo.com>
2022-10-23 01:58:26 -0300

    Register join time on catraca

--- plugins/catraca/src/main/java/com/doceazedo/catraca/identity/Username.kt
+++ plugins/catraca/src/main/java/com/doceazedo/catraca/identity/Username.kt
@@ -27,9 +27,19 @@ object Username {
             val data: JSONObject = resp.jsonObject
             Gson().fromJson(data["identity"].toString(), Identity::class.java)
         } catch (cause: Throwable) {
-            Bukkit.getLogger().warning("An error ocurred when trying to fetch ${Env.IDENTITY_URL}/api/v1/whois/$username")
-            cause.printStackTrace()
             null
+        }
+    }
+
+    fun registerJoin (username: String) {
+        try {
+            khttp.post(
+                "${Env.IDENTITY_URL.value}/api/v1/username/$username/join",
+                mapOf("Authorization" to "Bearer ${Env.IDENTITY_TOKEN.value}")
+            )
+        } catch (cause: Throwable) {
+            Bukkit.getLogger().warning("An error ocurred when trying to fetch /api/v1/username/$username/join")
+            cause.printStackTrace()
         }
     }
 }
--- plugins/catraca/src/main/java/com/doceazedo/catraca/utils/sendPlayer.kt
+++ plugins/catraca/src/main/java/com/doceazedo/catraca/utils/sendPlayer.kt
@@ -1,14 +1,16 @@
 package com.doceazedo.catraca.utils
 
 import com.doceazedo.catraca.Catraca
+import com.doceazedo.catraca.identity.Username.registerJoin
 import com.google.common.io.ByteStreams
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 
 val targetServer: String = Catraca.instance.config.getString("targetServer")
 
-fun sendPlayer (player: Player, server: String = targetServer) {
+fun sendPlayer (player: Player, server: String = targetServer, join: Boolean = true) {
     try {
+        if (join) registerJoin(player.displayName)
         val out = ByteStreams.newDataOutput()
         out.writeUTF("Connect")
         out.writeUTF(server)
