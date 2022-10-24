dbd246a81c62b8a4ca7dee9dd73629b74600ef95
doceazedo <me@doceazedo.com>
2022-10-23 23:26:41 -0300

    Update dashboard main page

--- apps/identity/src/routes/dashboard/+page.svelte
+++ apps/identity/src/routes/dashboard/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   import { PageTitle } from 'ssnkit';
   import { IDENTITY, logout } from "$lib/auth";
+
+  $: s = $IDENTITY.usernames.length > 1 ? 's' : '';
 </script>
 
-<PageTitle pretitle="Perfil" title={$IDENTITY?.primaryUsername} class="mb-6" />
-<pre class="card my-4">{JSON.stringify($IDENTITY, null, 2)}</pre>
+<PageTitle pretitle="Painel" title={$IDENTITY?.primaryUsername} class="mb-6" />
+
+<div class="notification is-warning is-light">
+  Você tem <b>{$IDENTITY.usernames.length} usuário{s}</b> cadastrado{s}.
+  Para criar mais um, acesse <a href="/dashboard/usernames">Minhas contas</a>.
+</div>
 
 <button class="button is-danger is-small" on:click={logout}>
   Desconectar
