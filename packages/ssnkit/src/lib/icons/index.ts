44b96ffef000ef01724066496cc10d35ff9e7b09
doceazedo <me@doceazedo.com>
2022-12-14 14:39:24 -0300

    Create website footer

--- packages/ssnkit/src/lib/icons/index.ts
+++ packages/ssnkit/src/lib/icons/index.ts
@@ -1,3 +1,5 @@
 export { default as CheckmarkIcon } from './checkmark-icon.svelte';
 export { default as DiscordAltIcon } from './discord-alt-icon.svelte';
 export { default as DiscordIcon } from './discord-icon.svelte';
+export { default as GitHubIcon } from './github-icon.svelte';
+export { default as TwitterIcon } from './twitter-icon.svelte';
--- packages/ssnkit/src/lib/layout/footer.svelte
+++ packages/ssnkit/src/lib/layout/footer.svelte
@@ -1,8 +1,88 @@
+<script lang="ts">
+  import { Github } from 'lucide-svelte';
+  import { DiscordIcon, GitHubIcon, TwitterIcon } from '../icons';
+  import { CopyIpTag } from '../elements';
+
+  const socials = [
+    {
+      icon: DiscordIcon,
+      href: 'https://discord.gg/DChTnVTuKp',
+    },
+    {
+      icon: TwitterIcon,
+      href: 'https://twitter.com/servidorsemnome',
+    },
+    {
+      icon: GitHubIcon,
+      href: 'https://github.com/doceazedo/ssn',
+    },
+  ];
+</script>
+
 <footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      Desenvolvido e gerenciado com 😡 por <a target="_blank" href="https://github.com/doceazedo">DoceAzedo</a>.
-      Licenciado sob <a target="_blank" href="https://opensource.org/licenses/GPL-3.0">GNU General Public License v3</a>.
-    </p>
+  <div class="footer-left">
+    <h1>ssn.gg</h1>
+    <p>DoceAzedo &copy; {new Date().getFullYear()}</p>
+  </div>
+
+  <div class="footer-center">
+    <h1>Pronto para jogar?</h1>
+    <CopyIpTag />
+  </div>
+
+  <div class="footer-right">
+    {#each socials as social}
+      <a href={social.href} target="_blank" class="button is-light is-small">
+        <svelte:component this={social.icon} />
+      </a>
+    {/each}
   </div>
 </footer>
+
+<style lang="sass">
+  .footer
+    display: flex
+    justify-content: space-between
+    padding: 5rem 2.5rem
+    background-color: #fff
+    border-top: 1px solid #e6e7ea
+
+    > *
+      width: 33.33333%
+
+    &-left
+      display: flex
+      flex-direction: column
+      justify-content: center
+
+      h1
+        font-size: 1.5rem
+
+    &-center
+      text-align: center
+
+      h1
+        margin-bottom: .25rem
+        font-size: 2rem
+        letter-spacing: -1.25px
+
+    &-right
+      display: flex
+      align-items: center
+      justify-content: flex-end
+      gap: .5rem
+
+      .button
+        width: 3rem
+        height: 3rem
+        padding: 0
+
+        :global(svg)
+          width: 1.5rem
+          height: 1.5rem
+
+    h1
+      font-family: 'Space Grotesk', sans-serif
+      font-weight: 700
+      line-height: 1.25
+</style>
