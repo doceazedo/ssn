58f46edf00ae705d08784935c54d6e7eb6b4a663
doceazedo <me@doceazedo.com>
2022-12-20 21:59:22 -0300

    Setup AntiBurrow

---
+++ plugins/antiburrow/src/main/java/com/doceazedo/antiburrow/AntiBurrow.kt
@@ -0,0 +1,13 @@
+package com.doceazedo.antiburrow
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AntiBurrow : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
