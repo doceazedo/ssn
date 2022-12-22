cbe2c150bfbf4a30e8579e8af7b6d3356a88e156
doceazedo <me@doceazedo.com>
2022-12-22 20:21:31 -0300

    Fix AntiBurrow once again 🙃

--- plugins/antiburrow/src/main/java/com/doceazedo/antiburrow/events/PlayerMove.kt
+++ plugins/antiburrow/src/main/java/com/doceazedo/antiburrow/events/PlayerMove.kt
@@ -6,22 +6,20 @@ import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
 
 object PlayerMove : Listener {
-    private val allowedBlocks = arrayOf(
-        Material.AIR,
-        Material.FENCE,
-        Material.FENCE_GATE,
-        Material.THIN_GLASS,
-        Material.STAINED_GLASS_PANE,
+    private val blockedBlocks = arrayOf(
+        Material.ENDER_CHEST,
+        Material.ANVIL,
+        Material.BEACON
     )
 
     @EventHandler
     fun onPlayerMove(e: PlayerMoveEvent) {
         val location = e.player.location
         val block = location.block
-        if (block.type in allowedBlocks) return
+        if (block.type == Material.AIR) return
 
         val heightThreshold = location.block.y + 0.1
-        if (block.type.isOccluding || (block.type.isSolid && location.y < heightThreshold)) {
+        if (location.y <= heightThreshold && (block.type.isOccluding || block.type in blockedBlocks)) {
             e.player.teleport(location.add(0.0, 1.0, 0.0))
         }
     }
