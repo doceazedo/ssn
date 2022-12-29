09d1e9bd016cb7868b597eda3c27b465c050506f
doceazedo <me@doceazedo.com>
2022-12-29 18:56:24 -0300

    Cache server status

--- apps/website/src/routes/api/v1/status/+server.ts
+++ apps/website/src/routes/api/v1/status/+server.ts
@@ -3,13 +3,21 @@ import { json } from "@sveltejs/kit";
 import type { RequestHandler } from '@sveltejs/kit';
 
 const ip = 'ssn.gg';
+const cooldown = 300000; // 5 minutes
+
+let status: JavaStatusResponse | null = null;
+let lastTimeFetched = new Date();
 
 export const GET: RequestHandler = async () => {
-  let status: JavaStatusResponse | null = null;
-  try {
-    status = await getServerStatus(ip);
-  } catch (e) { 
-    // you'll never see me ;)
+  // only fetch for status if last time fetch was >= 5 minutes ago
+  const now = Date.now();
+  if (status == null || now - lastTimeFetched.getTime() >= cooldown) {
+    try {
+      status = await getServerStatus(ip);
+      lastTimeFetched = new Date();
+    } catch (e) { 
+      status = null;
+    }
   }
 
   return json({
