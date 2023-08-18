06512d2dc56c724a879d3f23d0143aa865c60dcb
doceazedo <me@doceazedo.com>
2023-08-18 15:59:38 -0300

    Update references of 1.12.2 to 1.19.4

--- packages/ssnkit/src/lib/layout/sidebar.svelte
+++ packages/ssnkit/src/lib/layout/sidebar.svelte
@@ -1,28 +1,39 @@
 <script lang="ts">
-  import { Joystick, Server, ServerOff, Skull, Sprout, Twitter, Users } from 'lucide-svelte';
-  import { Card } from '../components';
-  import { DiscordAltIcon } from '../icons';
-  import type { JavaStatusResponse } from 'minecraft-server-util';
+  import {
+    Joystick,
+    Server,
+    ServerOff,
+    Skull,
+    Sprout,
+    Twitter,
+    Users,
+  } from "lucide-svelte";
+  import { Card } from "../components";
+  import { DiscordAltIcon } from "../icons";
+  import type { JavaStatusResponse } from "minecraft-server-util";
 
   export let status: JavaStatusResponse | null = null;
 </script>
 
 <Card
-  title={status ? 'Server online' : 'Server fechado'}
+  title={status ? "Server online" : "Server fechado"}
   icon={status ? Server : ServerOff}
 >
   <ul class="server-status">
-    <li><Users /> {status?.players?.online || 0}/{status?.players?.max || 0} online</li>
-    <li><Joystick /> Java Edition 1.12.2</li>
+    <li>
+      <Users />
+      {status?.players?.online || 0}/{status?.players?.max || 0} online
+    </li>
+    <li>
+      <Joystick />
+      <span>Java Edition 1.19.4 <small>Com suporte à 1.20.x</small></span>
+    </li>
     <li><Skull /> Pirata e original</li>
     <li><Sprout /> Seed: 372733000726032955</li>
   </ul>
 </Card>
 
-<Card
-  title="Discord"
-  icon={DiscordAltIcon}
->
+<Card title="Discord" icon={DiscordAltIcon}>
   <iframe
     class="widget"
     title="Discord widget"
@@ -33,27 +44,40 @@
   />
 </Card>
 
-<Card
-  title="Twitter"
-  icon={Twitter}
->
+<Card title="Twitter" icon={Twitter}>
   <div class="widget">
     <a
       class="twitter-timeline"
       href="https://twitter.com/servidorsemnome?ref_src=twsrc%5Etfw"
       data-height={512}
     >
-        Tweets by servidorsemnome
+      Tweets by servidorsemnome
     </a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
+    <script
+      async
+      src="https://platform.twitter.com/widgets.js"
+      charset="utf-8"></script>
   </div>
 </Card>
 
 <style lang="sass">
-  .server-status li
+  .server-status
     display: flex
-    align-items: center
-    gap: .5rem
+    flex-direction: column
+    gap: .25rem
+
+    li
+      display: flex
+      align-items: center
+      gap: .5rem
+
+    span
+      display: flex
+      flex-direction: column
+
+    small
+      font-size: .75rem
+      opacity: .75
 
   .widget
     width: 100%
