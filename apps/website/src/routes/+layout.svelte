3fdeec6ae0e8e13ccb52bf57d2af5668c02799c4
doceazedo <me@doceazedo.com>
2023-10-27 12:35:36 -0300

    Fix active navbar item

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-  import 'ssnkit/styles';
-  import { Layout } from 'ssnkit';
-  import { page } from '$app/stores';
-  import { env } from '$env/dynamic/public';
-  import { IDENTITY } from "$lib/auth";
-  import { SERVER_STATUS } from '$lib/status';
-  import { CURRENT_URL } from '$lib/stores';
-  import type { LayoutServerData } from './$types';
+	import 'ssnkit/styles';
+	import { Layout } from 'ssnkit';
+	import { page } from '$app/stores';
+	import { env } from '$env/dynamic/public';
+	import { IDENTITY } from '$lib/auth';
+	import { SERVER_STATUS } from '$lib/status';
+	import { CURRENT_URL } from '$lib/stores';
+	import type { LayoutServerData } from './$types';
 
-  export let data: LayoutServerData;
+	export let data: LayoutServerData;
 
-  $CURRENT_URL = $page.url.href.endsWith('/') ? $page.url.href.slice(0, -1) : $page.url.href;
+	$: currentUrl = $page.url.href.endsWith('/') ? $page.url.href.slice(0, -1) : $page.url.href;
+	$CURRENT_URL = currentUrl;
 
-  $IDENTITY = data.identity || null;
+	$IDENTITY = data.identity || null;
 </script>
 
 <Layout
-  currentUrl={$CURRENT_URL}
-  websiteBaseUrl={env.PUBLIC_WEBSITE_URL || ''}
-  identityBaseUrl={env.PUBLIC_IDENTITY_URL || ''}
-  identity={$IDENTITY}
-  playerCount={$SERVER_STATUS?.players?.online}
+	{currentUrl}
+	websiteBaseUrl={env.PUBLIC_WEBSITE_URL || ''}
+	identityBaseUrl={env.PUBLIC_IDENTITY_URL || ''}
+	identity={$IDENTITY}
+	playerCount={$SERVER_STATUS?.players?.online}
 >
-  <slot />
+	<slot />
 </Layout>
