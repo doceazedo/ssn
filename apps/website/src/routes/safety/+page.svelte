6a4800e6199504273bb31a9fbd7536d26c16aea6
doceazedo <me@doceazedo.com>
2024-02-28 18:27:58 -0300

    Update safety page

--- apps/website/src/routes/safety/+page.svelte
+++ apps/website/src/routes/safety/+page.svelte
@@ -12,22 +12,14 @@
 	<Card title="Autenticação" icon={Lock}>
 		<div class="content">
 			<p>
-				O SSN não tem contato com os servidores da Mojang, assim possibilitando que qualquer pessoa
-				acesse o servidor, mesmo que ela não tenha adquirido uma cópia original do jogo. Isso
-				significa que precisamos ter um sistema de autenticação próprio para garantir que somente
-				usuários autorizados tenham acesso às suas respectivas contas.
+				O SSN não usa o sistema da Mojang quando alguém entra no servidor. Isso significa que
+				qualquer um pode entrar, mesmo sem ter comprado o jogo. Por isso, precisamos de um jeito
+				diferente de saber quem é quem e garantir que ninguém use a conta de outra pessoa.
 			</p>
 			<p>
-				Um sistema de autenticação web também é mais seguro do que um sistema de login via chat
-				convencional, pois os dados de login são transmitidos e armazenados de forma segura em vez
-				de serem enviados via chat, o que pode ser interceptado por terceiros mal-intencionados.
-				Além disso, nosso sistema de autenticação permite que os usuários usem a mesma senha para
-				múltiplas contas, o que é mais conveniente do que ter que lembrar senhas diferentes para
-				cada nome de usuário.
-			</p>
-			<p>
-				Nosso sistema também nos permite ter um controle mais granular sobre quem pode se registrar
-				e acessar o servidor.
+				Nossa solução é entrar pelo site, que é mais seguro do que fazer login pelo chat, por
+				exemplo. Além disso, você pode usar a mesma senha para todas as suas contas no SSN e só
+				entrar uma vez, o que é mais fácil do que lembrar de várias senhas diferentes.
 			</p>
 		</div>
 	</Card>
@@ -35,39 +27,33 @@
 	<Card title="Login com Discord" icon={Key}>
 		<div class="content">
 			<p>
-				O registro via Discord usa a tecnologia oAuth 2.0, que é ainda mais seguro do que usar um
-				sistema de login e senha convencional, pois você não precisa lembrar de mais uma senha e nem
-				depende da segurança do SSN. O oAuth é um padrão aberto que permite que os usuários façam
-				login em aplicativos e sites usando suas contas de redes sociais, no caso, o Discord. Isso
-				significa que os usuários não precisam criar uma conta específica para o SSN, em vez disso,
-				podem usar sua conta do Discord já existente.
+				Entrar com o Discord é ainda mais seguro do que usar um e-mail e senha, porque você não
+				depende da segurança do SSN. O SSN <b>nunca</b> recebe sua senha do Discord, e funciona como
+				qualquer login usando redes sociais que outros sites também têm. O SSN só recebe a mensagem do
+				Discord que confirma que você é o dono da sua conta e que podemos te deixar passar.
 			</p>
+
 			<p>
-				Para garantir que o login via Discord é realmente seguro, é importante verificar se você já
-				está conectado à sua conta e que o site que está pedindo suas informações de login e senha
-				são realmente o <b>discord.com</b>.
-			</p>
-			<p>
-				Agora, é importante que você proteja a sua conta do Discord, para isso você deve usar uma
-				senha forte e única e ativar o múltiplo fator de autenticação. Isso adiciona uma camada
-				extra de segurança, exigindo que o usuário insira um código enviado para seu telefone ou
-				e-mail além da senha ao fazer login.
+				Agora, você precisará ter acesso à sua conta do Discord e proteger bem ela, por exemplo,
+				ativando a autenticação de dois fatores na sua conta.
 			</p>
 		</div>
 	</Card>
 
-	<Card title="Múltiplo fator de autenticação" icon={FormInput}>
+	<Card title="Autenticação de dois fatores" icon={FormInput}>
 		<div class="content">
 			<p>
-				Ativar o MFA, às vezes também referido como 2FA ou V2E, é a melhor maneira de aumentar a
-				segurança da sua conta em qualquer site. O MFA exige que o usuário forneça mais de uma forma
-				de autenticação, como uma senha e um código enviado para o telefone ou e-mail.
+				A autenticação de dois fatores (2FA ou MFA) é um código extra para acessar suas contas. Ela
+				torna sua conta ainda mais difícil de ser hackeada, mesmo que alguém descubra sua senha.
 			</p>
 			<p>
-				Se você usa o Discord para fazer login no SSN, basta acessar as configurações da sua conta
-				no Discord e habilitar o segundo fator de autenticação. Infelizmente, o sistema de
-				autenticação do SSN ainda não tem suporte para MFA. No entanto, estamos trabalhando para
-				implementar esse recurso em breve, para que você possa proteger ainda mais a sua conta.
+				Com o 2FA ativado, quando você entrar na sua conta, além da sua senha, você também precisará
+				digitar um código especial que é enviado para o seu celular. Esse código sempre muda, então
+				é impossível de alguém adivinhar.
+			</p>
+			<p>
+				O SSN ainda™ não tem o 2FA disponível. Mas mesmo assim, é importante ativar o 2FA no seu
+				Discord e e-mail para proteger suas contas.
 			</p>
 		</div>
 	</Card>
@@ -75,53 +61,42 @@
 	<Card title="Modificações de terceiros" icon={FileCog}>
 		<div class="content">
 			<p>
-				Modificações de terceiros, também chamados de "mods" ou "clients", são programas adicionais
-				que podem ser instalados no jogo para adicionar novas funcionalidades para auxiliar sua
-				gameplay no SSN. Enquanto os mods podem ser muito úteis, é importante tomar alguns cuidados
-				ao usá-los.
+				Mods podem adicionar funcionalidades e ferraments no seu jogo, e você pode usar quase
+				qualquer um desses mods no SSN. Mas é importante escolher os certos para não ter problemas.
 			</p>
-			<p>
-				Em primeiro lugar, é importante usar somente mods bem difundidos e populares, pois esses
-				tendem a ser mais confiáveis e ter menos problemas. Além disso, é importante baixar os mods
-				somente da fonte oficial, pois isso facilita que você obtenha uma versão segura e atualizada
-				do mod.
-			</p>
-			<p>
-				Se você tiver habilidades de programação, antes de instalar um mod, é uma boa ideia auditar
-				o código para verificar se não há nenhum problema de segurança. O ideal ainda é que você
-				faça sua própria build para evitar que ela tenha esses problemas.
-			</p>
-			<p>
-				Por fim, é importante nunca baixar arquivos, especialmente JARs e EXEs, enviados por outras
-				pessoas. Esses arquivos podem conter malwares que podem danificar seu computador ou roubar
-				suas informações pessoais. Em vez disso, baixe sempre os mods da fonte oficial e verifique o
-				código antes de instalá-los.
-			</p>
+			<ul>
+				<li>
+					<b>Use mods populares:</b> Procure mods que muitas pessoas usam e que são bem avaliados e baixe
+					apenas da fonte oficial.
+				</li>
+				<li>
+					<b>Se você sabe programar:</b> Verifique o código do mod antes de instalar. Faça sua própria
+					build do mod para ter certeza de que é segura.
+				</li>
+				<li>
+					<b>Cuidado com arquivos enviados por outras pessoas:</b> Nunca baixe arquivos de pessoas que
+					você não conhece, eles podem ter vírus que podem danificar seu computador.
+				</li>
+			</ul>
 		</div>
 	</Card>
 
 	<Card title="Proteja-se online" icon={ShieldCheck}>
 		<div class="content">
 			<p>
-				Para proteger sua segurança online, é importante ser cauteloso com as informações que você
-				compartilha na internet. Evite clicar em links que você não conhece e divulgar dados
-				pessoais com outras pessoas, mesmo que você acredite confiar nelas. Também é uma boa ideia
-				evitar compartilhar suas redes sociais com quem você não conhece bem, pois isso pode dar a
-				elas acesso a mais informações sobre você.
+				Tenha sempre cuidado com o que você compartilha no SSN, não clique em links estranhos, não
+				passe informações pessoais para ninguém e tome cuidado com quem você deixa ver suas redes
+				sociais. Mesmo pessoas que parecem de confiança podem não ser tão legais assim lá no fundo.
 			</p>
 			<p>
-				Se você estiver preocupado com sua segurança online, uma opção é apagar os rastros digitais
-				que você deixa na internet. Isso inclui desativar contas de redes sociais que você não usa
-				mais, privar perfis pessoais e excluir comentários e postagens antigas. Isso pode ajudar a
-				proteger sua privacidade e segurança online.
+				Lembre-se que você pode sempre ignorar pessoas que te provocam (use <code
+					>/ignore &lt;nome&gt;</code
+				> no chat), você não precisa responder com ofensas, pelo contrário, você poder ser legal com
+				os outros :)
 			</p>
 			<p>
-				Outra coisa a levar em consideração é não levar chacota de outros usuários a sério. Na
-				internet, algumas pessoas podem tentar se divertir às custas da reação negativa de outras,
-				mas é importante não deixar que isso afete você. Em vez disso, tente ignorar e não retribuir
-				esses comentários e, é claro, evite praticar esse mesmo comportamento com os outros. Ao
-				seguir essas dicas, você pode se proteger e manter um ambiente mais positivo e respeitoso na
-				internet.
+				O SSN pode ser um lugar legal se a atitude positiva partir de você e se você escolher bem
+				com quem quer jogar! Lembre-se que é só um jogo, vai se divertir!
 			</p>
 		</div>
 	</Card>
