8d5f862a38f103c3e263cefaba560496ddc5f917
DoceAzedo <me@doceazedo.com>
2024-06-02 10:29:47 -0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,16 @@ cd "$(dirname "$0")"
 source ../.env
 
 cd .. &&
+$DOCKER system prune -af --volumes &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d gateway warehouse warehouse-migrate &&
-$DOCKER builder prune -f &&
+$DOCKER system prune -af --volumes &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d website &&
-$DOCKER builder prune -f  &&
+$DOCKER system prune -af --volumes  &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d identity-web &&
-$DOCKER builder prune -f &&
+$DOCKER system prune -af --volumes &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d gatekeeper-redis gatekeeper-web &&
-$DOCKER builder prune -f &&
+$DOCKER system prune -af --volumes &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d melonbot rcon &&
-$DOCKER builder prune -f &&
+$DOCKER system prune -af --volumes &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d minecraft-proxy minecraft-queue minecraft-main &&
 ./scripts/discord-webhook.sh --maintenance-done
