b17f2ff9a80d16f1b20c455be8df037d4577c870
Doce <doce@Doces-MacBook-Pro-3.local>
2024-07-01 14:05:52 -0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,6 +16,10 @@ $DOCKER compose -f docker-compose.prod.yml up --build -d melonbot &&
 $DOCKER system prune -af --volumes &&
 $DOCKER compose -f docker-compose.prod.yml up --build -d rcon &&
 $DOCKER system prune -af --volumes &&
-$DOCKER compose -f docker-compose.prod.yml up --build -d minecraft-proxy minecraft-queue minecraft-main &&
+$DOCKER compose -f docker-compose.prod.yml up --build -d minecraft-proxy &&
+$DOCKER system prune -af --volumes &&
+$DOCKER compose -f docker-compose.prod.yml up --build -d minecraft-queue &&
+$DOCKER system prune -af --volumes &&
+$DOCKER compose -f docker-compose.prod.yml up --build -d minecraft-main &&
 $DOCKER system prune -af --volumes &&
 ./scripts/discord-webhook.sh --maintenance-done
