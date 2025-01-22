9e46899cf072ce92785929fdb25d2fbbb85fab25
DoceAzedo <me@doceazedo.com>
2025-01-22 10:20:04 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,10 @@ source ../.env
 
 cd .. &&
 $DOCKER system prune -a -f &&
-$DOCKER compose -f docker-compose.prod.yml up -d &&
+sleep .5 &&
+$DOCKER compose -f docker-compose.prod.yml up -d gateway identity-web warehouse warehouse-migrate gatekeeper-web gatekeeper-redis website melonbot &&
+sleep .5 &&
+$DOCKER system prune -a -f &&
+sleep .5 &&
+$DOCKER compose -f docker-compose.prod.yml up -d minecraft-proxy minecraft-queue minecraft-main &&
 ./scripts/discord-webhook.sh --maintenance-done
