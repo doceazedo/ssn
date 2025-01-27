2fe59b79ee548e86e3eb4d975caa5445488e3ab9
DoceAzedo <me@doceazedo.com>
2025-01-27 16:30:56 -0300

    add new website and pocketbase apps

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { i18n } from "$lib/i18n";
+	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
---
+++ apps/web/vite.config.ts
@@ -0,0 +1,16 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
+	server: {
+		allowedHosts: true
+	}
+});
